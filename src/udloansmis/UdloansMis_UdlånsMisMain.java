2a768eb59a7b4d418344b14d65ba24baadbc2020
hippomormor <cgenter@oncable.dk>
2016-04-21 16:18:18 +0200

    Lavet Look&Feel, så log-in vinduet ikke fucker op mellem Linux, Mac og Windows

--- src/udloansmis/UdloansMis_UdlånsMisMain.java
+++ src/udloansmis/UdloansMis_UdlånsMisMain.java
@@ -19,15 +19,16 @@ public class UdloansMis_UdlånsMisMain {
     
     public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException {
         try {
-            // Set cross-platform Java L&F (also called "Metal")
-        UIManager.setLookAndFeel(
-            UIManager.getCrossPlatformLookAndFeelClassName());
+            
+        // Set cross-platform Java look and feel (also called "Metal") - Blev nød til at ændre LookAndFeel, 
+        // så log-in vinduet ikke resizede mellem Windows, Linux og Mac
+        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
     } 
     catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
        e.printStackTrace();
     }
       
-        // Log-in (Din main bliver nu kørt fra "UdloansMis_LoginPanel", under "jButtonOKActionPerformed()" og "init()" efter log-in er accepteret af jacobs server);       
+        // Log-in (Din main bliver nu kørt fra "UdloansMis_LoginPanel", under "jButtonOKActionPerformed()" og "init()", efter log-in er accepteret af jacobs server og nøglerne er lavet og tjekket);       
         UdloansMis_LoginPanelCreator loginhandler = new UdloansMis_LoginPanelCreator();
         loginhandler.paintLogin();
     }
