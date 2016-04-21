66bccee670e4315507e4e23a9810ca6ce8ee2d08
hippomormor <cgenter@oncable.dk>
2016-04-21 18:19:53 +0200

    Removed System.out's

--- src/security/TokenHandler.java
+++ src/security/TokenHandler.java
@@ -17,14 +17,11 @@ public class TokenHandler {
     private BigInteger randomToken;
     private BigInteger publicToken;
     private BigInteger keyToken;
-    private final BigInteger credentials;
 
     // Hash credentials, reduce and generate random prime and token
     public TokenHandler(String user, String pass) {
-        credentials = new BigInteger(Integer.toString((user.hashCode() + pass.hashCode())/10000000-1));
-        System.out.println("Credentials.." + credentials);
         generateRandom();
-        generateToken();
+        generateToken(new BigInteger(Integer.toString((user.hashCode() + pass.hashCode())/10000000-1)));
     }
 
     // Generate random 7-bit prime number
@@ -35,7 +32,7 @@ public class TokenHandler {
     }
 
     // Generate token from random prime and hashed credentials: hashedCredentials^randomPrime (power)
-    private BigInteger generateToken() {
+    private BigInteger generateToken(BigInteger credentials) {
         publicToken = credentials.pow(Integer.parseInt(randomToken.toString()));
         System.out.println("Token generated.." + publicToken);
         return publicToken;
@@ -44,7 +41,7 @@ public class TokenHandler {
     // Generate key from recieved token and random prime: recievedToken^randomPrime (power)
     public BigInteger generateKey(BigInteger token) {
         keyToken = token.pow(Integer.parseInt(randomToken.toString()));
-        System.out.println("Key generated.." + keyToken.toString());
+        System.out.println("Key generated..");
         return keyToken;
     }
     
