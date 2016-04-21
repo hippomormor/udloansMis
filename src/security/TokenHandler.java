e53cacec290037a89e4e03da98f3b57a261e8c21
hippomormor <cgenter@oncable.dk>
2016-04-21 16:35:40 +0200

    Added comments

--- src/security/TokenHandler.java
+++ src/security/TokenHandler.java
@@ -17,8 +17,9 @@ public class TokenHandler {
     private BigInteger randomToken;
     private BigInteger publicToken;
     private BigInteger keyToken;
-    private BigInteger credentials;
+    private final BigInteger credentials;
 
+    // Hash credentials, reduce and generate random prime and token
     public TokenHandler(String user, String pass) {
         credentials = new BigInteger(Integer.toString((user.hashCode() + pass.hashCode())/10000000-1));
         System.out.println("Credentials.." + credentials);
@@ -26,40 +27,47 @@ public class TokenHandler {
         generateToken();
     }
 
+    // Generate random 7-bit prime number
     private BigInteger generateRandom() {
         randomToken = BigInteger.probablePrime(7, new SecureRandom());
         System.out.println("Random prime generated.." + randomToken);
         return randomToken;
     }
 
+    // Generate token from random prime and hashed credentials: hashedCredentials^randomPrime (power)
     private BigInteger generateToken() {
         publicToken = credentials.pow(Integer.parseInt(randomToken.toString()));
         System.out.println("Token generated.." + publicToken);
         return publicToken;
     }
 
+    // Generate key from recieved token and random prime: recievedToken^randomPrime (power)
     public BigInteger generateKey(BigInteger token) {
         keyToken = token.pow(Integer.parseInt(randomToken.toString()));
         System.out.println("Key generated.." + keyToken.toString());
         return keyToken;
     }
-
+    
+    // Check key with own key (should be equal)
     public boolean checkKey(BigInteger key) {
         if (key.toString().equals(keyToken.toString())) {
             return true;
         }
-        System.out.println("Key from server invalid");
+        System.out.println("Key  invalid");
         return false;
     }
 
+    // Get generated random prime
     public BigInteger getRandomToken() {
         return randomToken;
     }
 
+    // Get generated token
     public BigInteger getPublicToken() {
         return publicToken;
     }
     
+    // Get generated key
     public BigInteger getKeyToken() {
         return keyToken;
     }   
