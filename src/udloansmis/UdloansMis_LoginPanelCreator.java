93584da9a8cbc3be7f8d425a14147658da2b5f4f
hippomormor <cgenter@oncable.dk>
2016-04-23 18:18:54 +0200

    Tried to fix different sizes windows in OS X, Windows & Linux

--- src/udloansmis/UdloansMis_LoginPanelCreator.java
+++ src/udloansmis/UdloansMis_LoginPanelCreator.java
@@ -12,7 +12,11 @@ import java.awt.event.WindowEvent;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -24,49 +28,53 @@ public class UdloansMis_LoginPanelCreator {
     public JFrame vindue = new JFrame("UdlånsMis v1.0");
 
     public void paintLogin() throws NotBoundException, MalformedURLException, RemoteException {
-
-        // Create jPanel
-        loginPanel = new UdloansMis_LogInPanel();
-
-        // Add panel to window
-        vindue.add(loginPanel);
-
-        // Get screen size:
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenWidth = (int) screenSize.getWidth();
-        int screenHeight = (int) screenSize.getHeight();
-
-        // Make program quit if window is closed/removed, to avoid circumventing security
-        vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        // Remove frame & OS-specific theme
-        vindue.setUndecorated(true);
-
-        // Set allways on top     
-        vindue.setAlwaysOnTop(true);
-
-        // Set window size:   
-        vindue.setSize(233, 132);
-        //vindue.setSize((screenWidth / 10), (screenHeight / 10));
-
-        // Calculate centre:
-        vindue.setLocation(((screenWidth / 2) - vindue.getWidth() / 2), ((screenHeight / 2) - vindue.getHeight() / 2));
-
-        // Disable resizing
-        vindue.setResizable(false);
-
-        // Show window
-        vindue.setVisible(true);
-
-        // Add windowListener to check for window closing;
-        vindue.addWindowListener(new WindowAdapter() {
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-
-                // Close program if log-in window closes, to avoid circumventing security
-                System.exit(0);
-            }
-        });
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            // Create jPanel
+            loginPanel = new UdloansMis_LogInPanel();
+            
+            // Add panel to window
+            vindue.add(loginPanel);
+            
+            // Get screen size:
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int screenWidth = (int) screenSize.getWidth();
+            int screenHeight = (int) screenSize.getHeight();
+            
+            // Make program quit if window is closed/removed, to avoid circumventing security
+            vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            
+            // Remove frame & OS-specific theme
+            //vindue.setUndecorated(true);
+            
+            // Set allways on top
+            vindue.setAlwaysOnTop(true);
+            
+            // Set window size:
+            vindue.setSize(233, 132);
+            //vindue.setSize((screenWidth / 10), (screenHeight / 10));
+            
+            // Calculate centre:
+            vindue.setLocation(((screenWidth / 2) - vindue.getWidth() / 2), ((screenHeight / 2) - vindue.getHeight() / 2));
+            
+            // Disable resizing
+            vindue.setResizable(false);
+            
+            // Show window
+            vindue.setVisible(true);
+            
+            // Add windowListener to check for window closing;
+            vindue.addWindowListener(new WindowAdapter() {
+                
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    
+                    // Close program if log-in window closes, to avoid circumventing security
+                    System.exit(0);
+                }
+            });
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(UdloansMis_LoginPanelCreator.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
