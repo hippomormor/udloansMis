53140c05f08e78272c1fed32abe9aa4f423e76e0
hippomormor <cgenter@oncable.dk>
2016-05-03 15:55:28 +0200

    Added colors

--- src/udloansmis/UdloansMis_CellRenderer.java
+++ src/udloansmis/UdloansMis_CellRenderer.java
@@ -11,20 +11,29 @@ import javax.swing.table.DefaultTableCellRenderer;
  */
 public class UdloansMis_CellRenderer extends DefaultTableCellRenderer {
 
+    private final Color evenColor = Color.WHITE;
+    private final Color oddColor = new Color(242, 242, 242);
+
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
             boolean hasFocus, int row, int column) {
         Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        if (row % 2 == 0)
+            c.setBackground(evenColor);
+        else
+            c.setBackground(oddColor);
+        
+        if (!(value == null || value.toString().equals(""))) {
 
-        if (value == null || value.toString().equals("")) {
+            if (column == 5 && value.toString().equals("0"))
+                c.setBackground(new Color(250, 218, 100));
+            else if (column == 5 && Integer.parseInt(value.toString()) < 0) 
+                c.setBackground(new Color(244, 128, 110));      
+            else if (column == 5)
+                c.setBackground(new Color(184, 222, 133));
             return c;
         }
-        if (column == 5 && value.toString().equals("0") )
-            c.setForeground(Color.orange);
-        else if (column == 5 && Integer.parseInt(value.toString()) < 0)
-            c.setForeground(Color.red);   
-        else
-            c.setForeground(Color.black);
+
         return c;
 
     }
